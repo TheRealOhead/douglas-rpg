e1e71f1433bf0a0e45e3c531890042da44493ed3
Owen Parker <Oheadthaboss@gmail.com>
2021-10-08 09:42:30 -0400

    Added faces to dialog

--- dialog.js
+++ dialog.js
@@ -6,7 +6,13 @@ var dialog = {
 	textSpeed: 40, // Higher is slower
 	startCoords: [208,330],
 	charactersPerLine: 42,
-	pixelsBetweenLines: 22
+	pixelsBetweenLines: 22,
+	textActive: false,
+	currentlyTyping: false,
+
+	head:{
+		startCoords: [40,320]
+	}
 };
 
 
@@ -21,9 +27,10 @@ var dialog = {
 * @param {string} speaker Who is to say it
 */
 class Dialog {
-	constructor(content, speaker) {
+	constructor(content, speaker, emotion) {
 		this.content = content;
 		this.speaker = speaker;
+		this.emotion = emotion;
 
 		// This makes it easier to render out sequentally
 		this.contentArray = content.split('');
@@ -36,8 +43,26 @@ class Dialog {
 		// Render text box
 		ctx.drawImage(graphics.images['placeholderTextBox.png'], 0, c.height - graphics.images['placeholderTextBox.png'].height);
 
-		let index = 0;
-		this.renderCallback(index);
+		// Render face
+		let face = new Image();
+		face.src = 'images/dialogFaces/' + this.speaker + '/' + this.emotion + '.png';
+		
+		// Wait for image to load
+		face.addEventListener('load',()=>{
+
+			face.style.imageRendering = 'pixelated';
+
+			let index = 0;
+
+			// Turn on currentlyTyping
+			dialog.currentlyTyping = true;
+
+			// Draw the now-loaded face
+			ctx.drawImage(face,dialog.head.startCoords[0],dialog.head.startCoords[1],96,96);
+
+			this.renderCallback(index);
+
+		});
 	}
 	renderCallback(index) {
 
@@ -51,6 +76,9 @@ class Dialog {
 			index++;
 			audio.sounds['textBeep.wav'].play();
 			setTimeout(()=>{this.renderCallback(index)},dialog.textSpeed);
+		} else {
+			// Turn off currentlyTyping
+			dialog.currentlyTyping = false;
 		};
 	}
 }
@@ -69,12 +97,14 @@ class DialogList {
 	* Render out all the text boxes!
 	*/
 	render() {
+		dialog.textActive = true;
+
 		// To keep track
 		let index = 0;
 
 		// Function to be used for the "Enter" event
 		let eventFunc = (e)=>{
-			if (e.key == 'Enter') {
+			if (e.key == 'Enter' && !dialog.currentlyTyping) {
 				document.removeEventListener('keydown',eventFunc);
 				loop();
 			};
@@ -96,6 +126,15 @@ class DialogList {
 				// If it's the first text box, send a fake "Enter"
 				if (index == 0)
 					eventFunc({key:'Enter'});
+			} else {
+				// Closes the last text box
+				let enderFunc = (e) => {
+					if (e.key == 'Enter') {
+						dialog.textActive = false;
+						document.removeEventListener('keydown',enderFunc);
+					};
+				};
+				document.addEventListener('keydown',enderFunc);
 			};
 		};
 
@@ -103,3 +142,17 @@ class DialogList {
 		loop();
 	}
 }
+
+/// DEBUG POGGGGGGGG
+graphics.images['placeholderTextBox.png'].addEventListener('load',()=>{
+
+	let d = new DialogList([
+	new Dialog('Hello, my name is Mark Bellenoit!','mark','regular'),
+	new Dialog('Hang on a minute...','mark','sussy'),
+	new Dialog('Am I in a video game?!','mark','omg'),
+	new Dialog('I\'ll destroy you, Owen Parker! They will never find your remains! I\'m gonna chop you up and put you underneath the floorboards!','mark','angry')
+	]);
+
+	d.render();
+
+});
--- graphics.js
+++ graphics.js
@@ -1,10 +1,10 @@
+ctx.webkitImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false;
+
 let images = ['placeholderTextBox.png'];
 
-var graphics = {
-	images:{
-
-	}
-};
+var graphics = {images:{}};
 
 images.forEach((image)=>{
 	graphics.images[image] = new Image();
